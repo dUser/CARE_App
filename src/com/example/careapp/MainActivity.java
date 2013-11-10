90f4223c1ea23563ddd6664da5827bc7b2910bdd
dUser <user1324@server1324.fake>
2013-11-09 19:12:44 -0500

    Adds basic gui in the dash board pattern for top level app navigation.

--- src/com/example/careapp/MainActivity.java
+++ src/com/example/careapp/MainActivity.java
@@ -8,9 +8,10 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
