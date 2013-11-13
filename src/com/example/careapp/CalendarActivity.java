3dc6ec8679bc3212bf66f389bbf587b1b9034e11
NelsonLR <kimarimonku1@gmail.com>
2013-11-13 15:24:16 -0500

    Commiting calendar on laptop for desktop

--- src/com/example/careapp/CalendarActivity.java
+++ src/com/example/careapp/CalendarActivity.java
@@ -1,15 +1,43 @@
 package com.example.careapp;
 
+import java.util.GregorianCalendar;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Build;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Calendars;
+import android.provider.CalendarContract.Events;
+
+/*import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;*/
 
 public class CalendarActivity extends Activity {
 
+	private static final int PROJECTION_ID_INDEX = 0;
+	private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+	private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+	private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+	
+	public static final String[] EVENT_PROJECTION = new String[] {
+		Calendars._ID, // 0
+		Calendars.ACCOUNT_NAME, // 1
+		Calendars.CALENDAR_DISPLAY_NAME // 2
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,5 +79,72 @@ public class CalendarActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void onClick(View view) {
+		Intent calIntent = new Intent(Intent.ACTION_INSERT);
+		calIntent.setData(CalendarContract.Events.CONTENT_URI);
+		startActivity(calIntent);
 
+		Intent intent = new Intent(Intent.ACTION_INSERT);
+		intent.setType("vnd.android.cursor.item/event");
+		intent.putExtra(Events.TITLE, "Learn Android");
+		intent.putExtra(Events.EVENT_LOCATION, "Home suit home");
+		intent.putExtra(Events.DESCRIPTION, "Download Examples");
+
+		// Setting dates
+		GregorianCalendar calDate = new GregorianCalendar(2012, 10, 02);
+		intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+				calDate.getTimeInMillis());
+		intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+				calDate.getTimeInMillis());
+
+		// Make it a full day event
+		intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+
+		// Make it a recurring Event
+		intent.putExtra(Events.RRULE,
+				"FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
+
+		// Making it private and shown as busy
+		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
+		intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
+
+		startActivity(intent);
+
+	}
+	
+	public void queryCalendar(View view) {
+		// Run query
+		Cursor cur = null;
+		ContentResolver cr = getContentResolver();
+		Uri uri = Calendars.CONTENT_URI;
+		String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
+				+ Calendars.ACCOUNT_TYPE + " = ?))";
+
+		// Replace this with your own user and account type
+		String[] selectionArgs = new String[] { "Lars.Vogel@gmail.com",
+				"com.google" };
+		// Submit the query and get a Cursor object back.
+		cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+		Toast.makeText(this, String.valueOf(cur.getCount()), Toast.LENGTH_LONG)
+				.show();
+		// Use the cursor to step through the returned records
+		while (cur.moveToNext()) {
+			long calID = 0;
+			String displayName = null;
+			String accountName = null;
+
+			// Get the field values
+			calID = cur.getLong(PROJECTION_ID_INDEX);
+			displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+			accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+
+			// Do something with the values...
+			Toast.makeText(this, "Calendar " + displayName, Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+	
+
+	
 }
