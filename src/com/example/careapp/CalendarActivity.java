9ee0b2c45a990330264ed12d704d03d40e7e9abe
NelsonLR <kimarimonku1@gmail.com>
2013-11-14 00:16:48 -0500

    My work on the calendar activity. I'm stuck at the moment on actually getting the CARE's calendar until I get their username/email for it but I have a blank calendar to show up for demonstration purposes.
    Also chcek your email if you are getting build errors. Had to do some
    stuff with Google Tools for Google Calendar API but I got a fix if you
    are getting errors.

--- src/com/example/careapp/CalendarActivity.java
+++ src/com/example/careapp/CalendarActivity.java
@@ -1,54 +1,93 @@
 package com.example.careapp;
 
-import java.util.GregorianCalendar;
-
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
-import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Build;
 import android.provider.CalendarContract;
-import android.provider.CalendarContract.Calendars;
-import android.provider.CalendarContract.Events;
-
-/*import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.HttpTransport;  //Needed for query commented out block
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.*;*/
+import com.google.api.services.calendar.model.*;
 
 public class CalendarActivity extends Activity {
 
-	private static final int PROJECTION_ID_INDEX = 0;
-	private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-	private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-	private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-	
-	public static final String[] EVENT_PROJECTION = new String[] {
-		Calendars._ID, // 0
-		Calendars.ACCOUNT_NAME, // 1
-		Calendars.CALENDAR_DISPLAY_NAME // 2
-	};
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendar);
-		// Show the Up button in the action bar.
-		setupActionBar();
+		
+		long startMillis = 0;
+		
+		Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+		builder.appendPath("time");
+		ContentUris.appendId(builder, startMillis);
+		Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+		startActivity(intent);
+		
+		//Query for the CARE's Calendar. Need the actual info and sign it or it crashes the app. Empty calendar now for show.
+		/*private static final String DEBUG_TAG = "CalendarActivity";
+		public static final String[] INSTANCE_PROJECTION = new String[] {
+		    Instances.EVENT_ID,      // 0
+		    Instances.BEGIN,         // 1
+		    Instances.TITLE          // 2
+		  };
+		  
+		private static final int PROJECTION_ID_INDEX = 0;
+		private static final int PROJECTION_BEGIN_INDEX = 1;
+		private static final int PROJECTION_TITLE_INDEX = 2;
+
+
+		Calendar beginTime = Calendar.getInstance();
+		beginTime.set(2011, 9, 23, 8, 0);
+		long startMillis = beginTime.getTimeInMillis();
+		Calendar endTime = Calendar.getInstance();
+		endTime.set(2011, 10, 24, 8, 0);
+		long endMillis = endTime.getTimeInMillis();
+		  
+		Cursor cur = null;
+		ContentResolver cr = getContentResolver();
+
+		String selection = Instances.EVENT_ID + " = ?";
+		String[] selectionArgs = new String[] {"207"};
+
+		Uri.Builder builder = Instances.CONTENT_URI.buildUpon();
+		ContentUris.appendId(builder, startMillis);
+		ContentUris.appendId(builder, endMillis);
+
+		cur =  cr.query(builder.build(), 
+		    INSTANCE_PROJECTION, 
+		    selection, 
+		    selectionArgs, 
+		    null);
+		   
+		while (cur.moveToNext()) {
+		    String title = null;
+		    long eventID = 0;
+		    long beginVal = 0;    
+		    
+		    eventID = cur.getLong(PROJECTION_ID_INDEX);
+		    beginVal = cur.getLong(PROJECTION_BEGIN_INDEX);
+		    title = cur.getString(PROJECTION_TITLE_INDEX);
+		              
+		    Log.i(DEBUG_TAG, "Event:  " + title); 
+		    Calendar calendar = Calendar.getInstance();
+		    calendar.setTimeInMillis(beginVal);  
+		    DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		    Log.i(DEBUG_TAG, "Date: " + formatter.format(calendar.getTime()));    
+		    }
+		 }*/
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -67,84 +106,10 @@ public class CalendarActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void onClick(View view) {
-		Intent calIntent = new Intent(Intent.ACTION_INSERT);
-		calIntent.setData(CalendarContract.Events.CONTENT_URI);
-		startActivity(calIntent);
-
-		Intent intent = new Intent(Intent.ACTION_INSERT);
-		intent.setType("vnd.android.cursor.item/event");
-		intent.putExtra(Events.TITLE, "Learn Android");
-		intent.putExtra(Events.EVENT_LOCATION, "Home suit home");
-		intent.putExtra(Events.DESCRIPTION, "Download Examples");
-
-		// Setting dates
-		GregorianCalendar calDate = new GregorianCalendar(2012, 10, 02);
-		intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-				calDate.getTimeInMillis());
-		intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-				calDate.getTimeInMillis());
-
-		// Make it a full day event
-		intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-
-		// Make it a recurring Event
-		intent.putExtra(Events.RRULE,
-				"FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
-
-		// Making it private and shown as busy
-		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
-		intent.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY);
-
-		startActivity(intent);
-
-	}
-	
-	public void queryCalendar(View view) {
-		// Run query
-		Cursor cur = null;
-		ContentResolver cr = getContentResolver();
-		Uri uri = Calendars.CONTENT_URI;
-		String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
-				+ Calendars.ACCOUNT_TYPE + " = ?))";
-
-		// Replace this with your own user and account type
-		String[] selectionArgs = new String[] { "Lars.Vogel@gmail.com",
-				"com.google" };
-		// Submit the query and get a Cursor object back.
-		cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-		Toast.makeText(this, String.valueOf(cur.getCount()), Toast.LENGTH_LONG)
-				.show();
-		// Use the cursor to step through the returned records
-		while (cur.moveToNext()) {
-			long calID = 0;
-			String displayName = null;
-			String accountName = null;
-
-			// Get the field values
-			calID = cur.getLong(PROJECTION_ID_INDEX);
-			displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-			accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-
-			// Do something with the values...
-			Toast.makeText(this, "Calendar " + displayName, Toast.LENGTH_SHORT)
-					.show();
-		}
-	}
-	
-
 	
 }
